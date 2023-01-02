4fb6951fe4b64155947d8638796ecb1ef0ec6f4d
bardhshalaa <116592952+bardhshalaa@users.noreply.github.com>
2023-01-02 23:18:45 +0100

    added price slider to shopping page

--- js/product-functions.js
+++ js/product-functions.js
@@ -5,8 +5,53 @@ fetch("../../js/products.json")
     });
 
 let container = document.querySelector(".product-cards");
+initialItems = Math.floor(container.offsetWidth / 253);
 
-let initialItems = Math.floor(container.offsetWidth / 253);
+const rangeInput = document.querySelectorAll(".range-input input");
+priceInput = document.querySelectorAll(".price-input input");
+progress = document.querySelector(".slider .progress");
+
+let priceGap = 100;
+
+rangeInput.forEach(input => {
+    input.addEventListener("input", e => {
+        let minVal = parseInt(rangeInput[0].value),
+            maxVal = parseInt(rangeInput[1].value);
+
+        if (maxVal - minVal < priceGap) {
+            if (e.target.className === "range-min") {
+                rangeInput[0].value = maxVal - priceGap;
+            }
+            else {
+                rangeInput[1].value = minVal + priceGap;
+            }
+        } else {
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+
+    });
+});
+
+priceInput.forEach(input => {
+    input.addEventListener("input", e => {
+        let minVal = parseInt(priceInput[0].value),
+            maxVal = parseInt(priceInput[1].value);
+
+        if ((maxVal - minVal >= priceGap) && maxVal <= 1000 && minVal >=0 ) {
+            if (e.target.className === "input-min") {
+                rangeInput[0].value = minVal;
+                progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+            }
+            else {
+                rangeInput[1].value = maxVal;
+                progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    });
+});
 
 function loadFromCategory(category) {
     let products = JSON.parse(localStorage.getItem("products"));
